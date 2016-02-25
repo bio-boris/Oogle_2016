e337ed0c1ef953e2066a00d526cf2159300b23fc
Sadkhin <sadkhin2@taubh2.campuscluster.illinois.edu>
2016-02-25 15:37:07 -0600

    blast complete

---
+++ qsub_blast_zip.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# ----------------QSUB Parameters----------------- #
+#PBS -S /bin/bash
+#PBS -q aces
+#PBS -l nodes=1:ppn=12
+#PBS -l walltime=2:00:00
+#PBS -t 0-48
+# ----------------Load Modules-------------------- #
+# ----------------Your Commands------------------- #
+cd /home/sadkhin2/phytozome11/Oogle_2016
+mkdir logs
+./blast.pl $PBS_ARRAYID > logs/$PBS_ARRAYID.log 
+
